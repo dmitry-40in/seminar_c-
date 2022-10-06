9f1b257815579038bf61d90a3f0a1401dea83a03
DS <dmitry_40in@mail.ru>
2022-10-06 23:21:37 +0300

    task_010 done

--- task_010/Program.cs
+++ task_010/Program.cs
@@ -5,20 +5,32 @@
 // 918 -> 1
 
 Console.WriteLine();
-Console.WriteLine("Введите трехзначное число ");
+Console.WriteLine("Введите трехзначное число:");
 int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 
-// if (x % 100 == 0 || x % 1000 != 0) // сделать условие если больше чем трехзначное
-{
-int number2 = (x / 10) % 10;
+// if (Math.Abs(x) >= 100 && Math.Abs(x) <= 999)
+// {
+//     int number2 = Math.Abs((x / 10) % 10);
 
-Console.WriteLine($"Вторая цифра числа {x}: {number2}");
-Console.WriteLine();
-}
-else
+//     Console.WriteLine($"Вторая цифра числа {x}: {number2}");
+//     Console.WriteLine();
+// }
+// else
+// {
+//     Console.WriteLine($"Введеное число {x} не трехзначное");
+//     Console.WriteLine();
+// }
+
+bool Method3digitABS(int num)   // Создал возвратный метод на проверку трехзначности и использования отрицательных чисел
 {
-    Console.WriteLine($"Введеное число {x} не трехзначное");
-    Console.WriteLine();
+return Math.Abs(num) >= 100 && Math.Abs(num) <= 999;
 }
 
+int MethodSecondDigit(int numberP)
+{
+    return Math.Abs((numberP / 10) % 10);   // Создал возвратный метод выделение 2-ой цифры, и в отрицательных числах
+}
+
+if (Method3digitABS(x)) Console.WriteLine($"Вторая цифра числа {x}: {MethodSecondDigit(x)}");
+else Console.WriteLine($"Введеное число {x} не трехзначное");
