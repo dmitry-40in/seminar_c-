8589ded3137742d2012d034a5abf3de9569fa83c
DS <dmitry_40in@mail.ru>
2022-10-21 13:05:12 +0300

    task_041 & task_043 - all done

---
+++ task_041/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223 -> 3
+
+Console.WriteLine();
+Console.Write("Сколько чисел будите вводить: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[] array = new int[number];
+array = FillArray(array);
+
+int numbers = NumbersGreaterThanZero(array);
+
+PrintArray(array);
+Console.WriteLine($" -> {numbers}");
+
+
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите число №{i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arr.Length; i++) Console.Write(i < arr.Length - 1 ? $"{arr[i]}, " : $"{arr[i]}");
+}
+
+
+int NumbersGreaterThanZero(int[] arr)
+{
+    int count = default;
+    for (int i = 0; i < arr.Length; i++) count = arr[i] > 0 ? count + 1 : count;
+    return count;
+}
---
+++ task_043/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 43. Напишите программу, которая найдет точку
+// пересечения двух прямых, заданных уравнениями y = k1 * x + b1
+// y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,5)
+
+Console.Write("Введите значение b1: ");
+double numB1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+double numK1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double numB2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+double numK2 = Convert.ToDouble(Console.ReadLine());
+
+double numX = FindX(numB1, numK1, numB2, numK2);
+double numY = FindY(numX, numK1, numB1);
+
+Console.WriteLine();
+Console.WriteLine($"b1 = {numB1}, k1 = {numK1}, b2 = {numB2}, k2 = {numK2} -> ({numX}; {numY})");
+
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 1);
+    return x;
+}
+
+double FindY(double x, double k1, double b1)
+{
+    double y = Math.Round(k1 * x + b1, 1);
+    return y;
+}
+
+
